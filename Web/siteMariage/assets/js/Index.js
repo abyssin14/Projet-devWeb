4c0ff30788723850b45f0573deebf51c3fdbbfc5
Tristan Pestiaux <45734363+abyssin14@users.noreply.github.com>
2020-03-13 11:39:04 +0100

    lien api react

--- Web/siteMariage/assets/js/Index.js
+++ Web/siteMariage/assets/js/Index.js
@@ -1,18 +1,60 @@
 // ./src/js/app.js
     
 import React, { Component } from 'react';
-import '../css/App.css';
+import '../css/app.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class Index extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8000/api/cadeaux")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Remarque : il est important de traiter les erreurs ici
+                // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+                // des exceptions provenant de réels bugs du composant.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
 
     render() {
-    return (
-      <span>Ceci est du react</span>
-    )
-
-  }
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Erreur : {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Chargement…</div>;
+        } else {
+            return (
+                <ul>
+                {items.map(item => (
+                        <li key={item.id}>
+                    {item.nom} {item.prix}
+        </li>
+        ))}
+        </ul>
+        );
+        }
+    }
 }
   
   export default Index;
--- Web/siteMariage/assets/js/app.js
+++ Web/siteMariage/assets/js/app.js
@@ -4,7 +4,6 @@ import '../css/app.css';
 import Index from './Index';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ReactOnRails from 'react-on-rails';
 
 
 ReactDOM.render(
@@ -12,8 +11,6 @@ ReactDOM.render(
 <Index/>
 </BrowserRouter>
     , document.getElementById('root'));
-
-ReactOnRails.register({ Index });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
