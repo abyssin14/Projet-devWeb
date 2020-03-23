8a83d99f3517ba4920ea61ce3e075fc41eb566b7
Jayli <45418688+VludZ45@users.noreply.github.com>
2020-03-23 22:23:25 +0100

    Form api invite
    Form api invite

--- Web/siteMariage/assets/js/Components/Formulaire.js
+++ Web/siteMariage/assets/js/Components/Formulaire.js
@@ -1,16 +1,90 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useForm } from 'react-hook-form'
-import FormInvites from '../Containers/FormInvites';
+import { number,Integer } from 'prop-types';
+
 
 class Formulaire extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      allergie: new String(),
+      accompagnant: false,
+      enfant: 0,
+      presentCeremonie: false,
+      presentVinDHonneur: false,
+      presentRepas: false,
+      presentSoiree: false,
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.name === 'presentCeremonie' || target.name === 'accompagnant' || target.name === 'presentVinDHonneur' || target.name === 'presentRepas' || target.name === 'presentSoiree' ? target.checked : target.value;
+    const name = target.name; 
+    
+    this.setState({
+      [name]: value
+    });
+ 
+    
+
+  }
+  
+
+handleSubmit(event) {
+  fetch('http://localhost:8000/api/invites', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    "allergie": this.state.allergie,
+    "accompagnant": this.state.accompagnant,
+    "enfant": parseInt(this.state.enfant),
+    "presentCeremonie": this.state.presentCeremonie,
+    "presentVinDHonneur": this.state.presentVinDHonneur,
+    "presentRepas": this.state.presentRepas,
+    "presentSoiree": this.state.presentSoiree
+  })
+})
+}
 
   render(){
     return(
   <div>
-  <br></br><br></br><br></br>
-  <FormInvites />
+    <br></br><br></br><br></br>
+  <form onSubmit={this.handleSubmit}>
+      <label>Formulaire de présence</label><br></br>
+      <label>Veuillez cocher les moments du mariage auxquels vous serez présents</label>
+      <br></br>
+      <div>
+      <input type="checkbox" name="presentCeremonie" checked={this.state.presentCeremonie}  onChange={this.handleInputChange}></input>
+      <label>Cérémonie</label>
+      <input type="checkbox" name="presentRepas" checked={this.state.presentRepas} onChange={this.handleInputChange}></input>
+      <label>Repas</label>
+      <br></br>
+      <input type="checkbox" name="presentVinDHonneur" checked={this.state.presentVinDHonneur} onChange={this.handleInputChange}></input>
+      <label>Vin d'honneur</label>
+      <input type="checkbox"  name="presentSoiree" checked={this.state.presentSoiree} onChange={this.handleInputChange}></input>
+      <label>Soirée</label>
+      </div>
+      <br></br>
+      <label>Allergie</label><br></br>
+      <input type="text" name="allergie" value={this.state.allergie} onChange={this.handleInputChange}></input>
+      <br></br><br></br>
+      <input type="checkbox" name="accompagnant" checked={this.state.accompagnant} onChange={this.handleInputChange}></input>
+      <label>Serez-vous accompagnez?</label><br></br>
+      <label>Enfant</label><br></br>
+      <input type="number"  min="0" name="enfant" value={this.state.enfant} onChange={this.handleInputChange}></input>
+      <br></br>
+      <input type="submit" value="Envoyer"></input>
+  </form>
+
   </div>
     )
   }
