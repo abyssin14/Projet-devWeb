81be241ac17b9a6462efc91e4cd8ff172e09f781
Jayli <45418688+VludZ45@users.noreply.github.com>
2020-04-28 14:40:00 +0200

    Edit cadeau

--- Web/siteMariage/assets/js/Components/Administration.js
+++ Web/siteMariage/assets/js/Components/Administration.js
@@ -6,6 +6,10 @@ import { number } from 'prop-types';
 import fleur from "../../img/FondFleurs.png"
 import fleurs from "../../img/Fleurs.png"
 import { getCadeaux, getInvites, putCadeau } from "../Utils/fetching.js"
+import ReactDOM from 'react-dom';
+
+
+    
 
 class Administration extends Component {
 
@@ -18,11 +22,16 @@ class Administration extends Component {
             nomEdit: new String(),
             prixEdit: new String(),
             descEdit: new String(),
-            
+            recolteEdit: [],
+            achteurEdit: [],
+            idEdit: Number()   
         };
 
         this.editCadeau = this.editCadeau.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.suppInvestisseur = this.suppInvestisseur.bind(this);
+        this.updateCadeau = this.updateCadeau.bind(this);
+    
 
     }
 
@@ -55,20 +64,65 @@ class Administration extends Component {
     
         this.setState({
           [name]: value
-        });
-    
-    
-    
+        });  
       }
+      
+      suppInvestisseur(indexAsupprimer) {
+        var tableauAcheteur = this.state.achteurEdit;
+        tableauAcheteur.splice(indexAsupprimer, 1);
+        this.state.achteurEdit = tableauAcheteur;
+
+        var tableauRecolte = this.state.recolteEdit;
+        tableauRecolte.splice(indexAsupprimer,1);
+        this.state.recolteEdit = tableauRecolte;
+         
+              }
 
     // Vue edit cadeau
-    editCadeau(cadeauNom, cadeauPrix, cadeauID, cadeauDesc) {
-   
-        
-        document.getElementById("cadeauToEdit").innerHTML = " Editer le cadeau " + cadeauNom;
-        
-    }
+    editCadeau(cadeauNom, cadeauPrix, cadeauID, cadeauDesc, acheteurs,recoltes) {
 
+        document.getElementById('editCadeau').style.display = "block";
+      
+        this.setState({
+            nomEdit: cadeauNom,
+            prixEdit: cadeauPrix,
+            descEdit: cadeauDesc
+          });  
+
+        this.state.idEdit = cadeauID;
+        this.state.recolteEdit = recoltes;
+        this.state.achteurEdit = acheteurs;
+        document.getElementById("investisseurs").innerHTML = "";
+        document.getElementById("cadeauToEdit").innerHTML = " Editer le cadeau " + cadeauNom;
+        const listeInvestisseurs = [];
+        for ( var i = 0; i < acheteurs.length; i++) {
+          
+        listeInvestisseurs.push( <tr>
+           <td>  {acheteurs[i]}</td> 
+            <td> {recoltes[i]}</td>
+           <td><button className="btn btn-danger" style={{display: "inline-block"}} onClick={this.suppInvestisseur.bind(this,i)}>Supprimer</button></td>
+           </tr>
+        )
+            }
+            ReactDOM.render(listeInvestisseurs, document.getElementById('investisseurs'));
+        }
+
+
+        updateCadeau() {
+
+
+            var body = JSON.stringify({
+                "nom": this.state.nomEdit,
+                "prix": parseInt(this.state.prixEdit),
+                "description": this.state.descEdit,
+                "acheteurs":  this.state.achteurEdit ,
+                "montantsRecoltes": this.state.recolteEdit ,
+                "payement": "enattente"
+              });
+          putCadeau(this.state.idEdit, body);
+        }
+       
+      
 
     render(){
 
@@ -81,10 +135,10 @@ class Administration extends Component {
         } else {
 
         return(
-          <div className="w3-grayscale-min fondFormulaire" style={{width:"100%", height:"100%",backgroundColor:"rgb(255, 255, 204)"}}>
+          <div className="w3-grayscale-min fondFormulaire" style={{width:"100%", backgroundColor:"rgb(255, 255, 204)"}}>
       
 
-      <div className="container mt-4" id="EditCadeau">
+      <div className="container mt-4" id="gererCadeau">
     <h1>Gérer les cadeaux</h1>
     <table className="table table-striped">
         <thead><tr>
@@ -106,7 +160,7 @@ class Administration extends Component {
 
 <td> </td>
 <td>
-    <a className="btn btn-secondary" style={{marginRight:"6px"}} onClick={this.editCadeau.bind(this, item.nom, item.prix, item.id, item.description)}>Editer</a>
+    <a className="btn btn-secondary" style={{marginRight:"6px"}} onClick={this.editCadeau.bind(this, item.nom, item.prix, item.id, item.description,item.acheteurs,item.montantsRecoltes)}>Editer</a>
     <button className="btn btn-danger" style={{display: "inline-block"}}>Supprimer</button>
         
 
@@ -128,12 +182,20 @@ class Administration extends Component {
 
 
 
-<div className="container mt-4">
+<div className="container mt-4" id="editCadeau" style={{display:"none"}}>
 <h1 id="cadeauToEdit"></h1>
 <label></label><br></br>
       <input type="text" placeholder="Nom" name="nomEdit" className="form-control w-25" value={this.state.nomEdit} onChange={this.handleInputChange}></input>
       <input type="text" placeholder="Prix" name="prixEdit" className="form-control w-25" value={this.state.prixEdit} onChange={this.handleInputChange}></input>
       <input type="text" placeholder="Description" name="descEdit" className="form-control w-25" value={this.state.descEdit} onChange={this.handleInputChange}></input>
+      <table className="table table-striped" id="investisseurs">
+          
+         
+      </table>
+      <div className="text-right">
+        <a className="btn btn-primary" onClick={this.updateCadeau.bind(this)}>Confirmer</a>
+    </div>
+   
 </div>
 
 
